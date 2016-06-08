c4b6e6b9e10a6b444e929e88808e9029f12ec227
Artur <artur.prado@gmail.com>
2016-06-08 10:23:49 -0300

    Adjust field names and urls to match the code refactor new behavior

--- content.js
+++ content.js
@@ -53,49 +53,49 @@ chrome.storage.sync.get(null, function(items) {
     });
   }
 
-  if (path === '/auto/quotes/new') {
-    fill_in('auto_quote[lead_person_attributes][name]', getValue('leadName'));
-    fill_in('auto_quote[lead_person_attributes][phone]', getValue('leadPhone'));
-    fill_in('auto_quote[lead_person_attributes][email]', getValue('leadEmail'));
+  if (path === '/auto/order/edit' || path === '/auto/order/lead_person_data') {
+    fill_in('auto_order_flow_lead_person_data[lead_person_attributes][name]', getValue('leadName'));
+    fill_in('auto_order_flow_lead_person_data[lead_person_attributes][phone]', getValue('leadPhone'));
+    fill_in('auto_order_flow_lead_person_data[lead_person_attributes][email]', getValue('leadEmail'));
   }
 
-  if (path === '/auto/quotes/vehicles/edit') {
-    select('auto_quote[vehicle][make]', getValue('vehicleMake'));
+  if (path === '/auto/order/pricing_requirements/edit' || path === '/auto/order/pricing_requirements') {
+    select('auto_order_flow_pricing_requirements[vehicle_attributes][make]', getValue('vehicleMake'));
     setTimeout(function(){
-      select('auto_quote[vehicle][model]', getValue('vehicleModel'));
+      select('auto_order_flow_pricing_requirements[vehicle_attributes][model]', getValue('vehicleModel'));
       setTimeout(function(){
-        select('auto_quote[vehicle][year]', getValue('vehicleYear'));
+        select('auto_order_flow_pricing_requirements[vehicle_attributes][year]', getValue('vehicleYear'));
         setTimeout(function(){
-          select('auto_quote[vehicle][version]', getValue('vehicleVersion'));
-          select('auto_quote[vehicle][brand_new]', getValue('vehicleBrandNew'));
-          select('auto_quote[vehicle][usage]', getValue('vehicleUsage'));
-          select('auto_quote[vehicle][purchased]', getValue('vehiclePurchased'));
-          select('auto_quote[vehicle][collected]', getValue('vehicleCollected'));
-          fill_in('auto_quote[vehicle_address][cep]', getValue('vehicleCep'));
-          select('auto_quote[vehicle][collected]', getValue('vehicleCollected'));
-          select('auto_quote[driver][gender]', getValue('driverGender'));
-          fill_in('auto_quote[driver][date_of_birth]', getValue('driverDateOfBirth'));
+          select('auto_order_flow_pricing_requirements[vehicle_attributes][version]', getValue('vehicleVersion'));
+          select('auto_order_flow_pricing_requirements[vehicle_attributes][brand_new]', getValue('vehicleBrandNew'));
+          select('auto_order_flow_pricing_requirements[vehicle_attributes][usage]', getValue('vehicleUsage'));
+          select('auto_order_flow_pricing_requirements[vehicle_attributes][purchased]', getValue('vehiclePurchased'));
+          select('auto_order_flow_pricing_requirements[vehicle_attributes][collected]', getValue('vehicleCollected'));
+          fill_in('auto_order_flow_pricing_requirements[vehicle_attributes][address_attributes][zipcode]', getValue('vehicleCep'));
+          select('auto_order_flow_pricing_requirements[vehicle_attributes][collected]', getValue('vehicleCollected'));
+          select('auto_order_flow_pricing_requirements[driver_attributes][gender]', getValue('driverGender'));
+          fill_in('auto_order_flow_pricing_requirements[driver_attributes][date_of_birth]', getValue('driverDateOfBirth'));
           trigger('driver_date_of_birth');
-          select('auto_quote[driver][years_since_last_claim]', getValue('driverLastClaim'));
+          select('auto_order_flow_pricing_requirements[driver_attributes][years_since_last_claim]', getValue('driverLastClaim'));
         }, delay);
       }, delay);
     }, delay);
   }
 
-  if (path === '/auto/proposals/insured_people/edit' || path === '/auto/proposals/insured_people') {
-    fill_in('auto_quote[insured_person][name]', getValue('insuredPersonName'));
-    fill_in('auto_quote[insured_person][cpf]', getValue('insuredPersonCpf'));
-    setJobRole('auto_quote[insured_person][job_role_name]', getValue('insuredPersonJobRole'));
-    select('auto_quote[insured_person][salary_range]', getValue('insuredPersonSalaryRange'));
-    fill_in('auto_quote[insured_person_address][number]', getValue('insuredPersonAddressNumber'));
+  if (path === '/auto/order/insured_person_data/edit' || path === '/auto/order/insured_person_data') {
+    fill_in('auto_order_flow_insured_person_data[insured_person_attributes][name]', getValue('insuredPersonName'));
+    fill_in('auto_order_flow_insured_person_data[insured_person_attributes][cpf]', getValue('insuredPersonCpf'));
+    setJobRole('auto_order_flow_insured_person_data[insured_person_attributes][occupation]', getValue('insuredPersonJobRole'));
+    select('auto_order_flow_insured_person_data[insured_person_attributes][salary_range]', getValue('insuredPersonSalaryRange'));
+    fill_in('auto_order_flow_insured_person_data[insured_person_attributes][address_attributes][number]', getValue('insuredPersonAddressNumber'));
   }
 
-  if (path === '/auto/proposals/vehicles/edit') {
-    fill_in('auto_quote[vehicle][license_plate]', getValue('vehicleLicensePlate'));
-    select('auto_quote[vehicle][bullet_proof]', getValue('vehicleBulletProof'));
+  if (path === '/auto/order/vehicle_data/edit' || path === '/auto/order/vehicle_data') {
+    fill_in('auto_order_flow_vehicle_data[vehicle_attributes][license_plate]', getValue('vehicleLicensePlate'));
+    select('auto_order_flow_vehicle_data[vehicle_attributes][bullet_proof]', getValue('vehicleBulletProof'));
   }
 
-  if (path === '/auto/payments/new' || path === '/home/proposals/payments/new' || path === '/life/proposals/payments/new') {
+  if (path === '/auto/order/payment_data/edit' || path === '/home/order/payment_data/edit' || path === '/life/order/payment_data/edit') {
     fill_in('credit_card_payment[credit_card][number]', '4111 1111 1111 1111');
     fill_in('credit_card_payment[credit_card][name]', 'John Doe');
     select('credit_card_payment[credit_card][due_date_month]', '5');
@@ -104,61 +104,58 @@ chrome.storage.sync.get(null, function(items) {
     $('[name="credit_card_payment[terms_of_service]"]').prop('checked', true);
   }
 
-  if (path === '/home/quotes/new') {
-    fill_in('home_quote[lead_person_attributes][name]', getValue('leadName'));
-    fill_in('home_quote[lead_person_attributes][phone]', getValue('leadPhone'));
-    fill_in('home_quote[lead_person_attributes][email]', getValue('leadEmail'));
+  if (path === '/home/order/edit') {
+    fill_in('home_order_flow_pricing_requirements[lead_person_attributes][name]', getValue('leadName'));
+    fill_in('home_order_flow_pricing_requirements[lead_person_attributes][phone]', getValue('leadPhone'));
+    fill_in('home_order_flow_pricing_requirements[lead_person_attributes][email]', getValue('leadEmail'));
 
-    select('home_quote[property_type_usage]', getValue('propertyTypeUsage'));
-    fill_in('home_quote[property_price]', getValue('propertyValue'));
-    fill_in('home_quote[property_cep]', getValue('propertyCep'));
+    select('home_order_flow_pricing_requirements[property_attributes][type_usage]', getValue('propertyTypeUsage'));
+    fill_in('home_order_flow_pricing_requirements[property_attributes][price]', getValue('propertyValue'));
+    fill_in('home_order_flow_pricing_requirements[property_attributes][address_attributes][zipcode]', getValue('propertyCep'));
   }
 
-  if (path === '/home/proposals/insured_people/edit') {
-    select('home_proposal[insured_person][gender]', getValue('insuredPersonGender'));
-    fill_in('home_proposal[insured_person][cpf]', getValue('insuredPersonCpf'));
-    fill_in('home_proposal[insured_person][date_of_birth]', getValue('insuredPersonDateOfBirth'));
-    setJobRole('home_proposal[insured_person][job_role_name]', getValue('insuredPersonJobRole'));
-    select('home_proposal[insured_person][salary_range]', getValue('insuredPersonSalaryRange'));
+  if (path === '/home/order/insured_person_data/edit') {
+    fill_in('home_order_flow_insured_person_data[insured_person_attributes][name]', getValue('insuredPersonName'));
+    select('home_order_flow_insured_person_data[insured_person_attributes][gender]', getValue('insuredPersonGender'));
+    fill_in('home_order_flow_insured_person_data[insured_person_attributes][cpf]', getValue('insuredPersonCpf'));
+    fill_in('home_order_flow_insured_person_data[insured_person_attributes][date_of_birth]', getValue('insuredPersonDateOfBirth'));
+    setJobRole('home_order_flow_insured_person_data[insured_person_attributes][occupation]', getValue('insuredPersonJobRole'));
+    select('home_order_flow_insured_person_data[insured_person_attributes][salary_range]', getValue('insuredPersonSalaryRange'));
   }
 
-  if (path === '/home/proposals/properties/edit') {
-    fill_in('home_proposal[property_address][number]', getValue('insuredPersonAddressNumber'));
+  if (path === '/home/order/property_data/edit') {
+    fill_in('home_order_flow_property_data[property_attributes][address_attributes][number]', getValue('insuredPersonAddressNumber'));
   }
 
-  if (path === '/life/quotes/new') {
-    fill_in('life_quote[lead_person_attributes][name]', getValue('leadName'));
-    fill_in('life_quote[lead_person_attributes][phone]', getValue('leadPhone'));
-    fill_in('life_quote[lead_person_attributes][email]', getValue('leadEmail'));
-    fill_in('life_quote[insured_person][date_of_birth]', getValue('insuredPersonDateOfBirth'));
+  if (path === '/life/order/edit') {
+    fill_in('life_order_flow_pricing_requirements[lead_person_attributes][name]', getValue('leadName'));
+    fill_in('life_order_flow_pricing_requirements[lead_person_attributes][phone]', getValue('leadPhone'));
+    fill_in('life_order_flow_pricing_requirements[lead_person_attributes][email]', getValue('leadEmail'));
+    fill_in('life_order_flow_pricing_requirements[insured_person_attributes][date_of_birth]', getValue('insuredPersonDateOfBirth'));
     trigger('insured_person_date_of_birth');
-    setJobRole('life_quote[insured_person][job_role_name]', getValue('insuredPersonJobRole'));
-    select('life_quote[insured_person][salary_range]', getValue('insuredPersonSalaryRange'));
-    select('life_quote[insured_person][has_mate]', getValue('insuredPersonHasMate'));
-    if (getValue('insuredPersonHasMate') === 'yes') {
-      fill_in('life_quote[insured_person_mate][date_of_birth]', getValue('insuredPersonMateDateOfBirth'));
+    setJobRole('life_order_flow_pricing_requirements[insured_person_attributes][occupation]', getValue('insuredPersonJobRole'));
+    select('life_order_flow_pricing_requirements[insured_person_attributes][salary_range]', getValue('insuredPersonSalaryRange'));
+    select('life_order_flow_pricing_requirements[insured_person_attributes][has_mate]', getValue('insuredPersonHasMate'));
+    if (getValue('insuredPersonHasMate') === 'true') {
+      fill_in('life_order_flow_pricing_requirements[insured_person_mate_attributes][date_of_birth]', getValue('insuredPersonMateDateOfBirth'));
       trigger('insured_person_mate_date_of_birth');
-      setJobRole('life_quote[insured_person_mate][job_role_name]', getValue('insuredPersonMateJobRole'));
+      setJobRole('life_order_flow_pricing_requirements[insured_person_mate_attributes][occupation]', getValue('insuredPersonMateJobRole'));
     }
   }
 
-  if (path === '/life/proposals/insured_people/edit') {
-    fill_in('life_quote[insured_person][name]', getValue('insuredPersonName'));
-    fill_in('life_quote[insured_person][cpf]', getValue('insuredPersonCpf'));
+  if (path === '/life/order/insured_person_data/edit') {
+    fill_in('life_order_flow_insured_person_data[insured_person_attributes][name]', getValue('insuredPersonName'));
+    fill_in('life_order_flow_insured_person_data[insured_person_attributes][cpf]', getValue('insuredPersonCpf'));
     trigger('insured_person_cpf');
-    select('life_quote[insured_person][gender]', getValue('insuredPersonGender'));
-    fill_in('life_quote[address][cep]', getValue('insuredPersonCep'));
+    select('life_order_flow_insured_person_data[insured_person_attributes][gender]', getValue('insuredPersonGender'));
+    fill_in('life_order_flow_insured_person_data[insured_person_attributes][address_attributes][zipcode]', getValue('insuredPersonCep'));
     trigger('mailing_address_cep');
-    fill_in('life_quote[address][number]', getValue('insuredPersonAddressNumber'));
-    fill_in('life_quote[address][full_address]', 'XPTO');
-    fill_in('life_quote[address][neighborhood]', 'Jardins');
-    fill_in('life_quote[address][city]', 'SAO PAULO');
-    fill_in('life_quote[address][state]', 'SP');
+    fill_in('life_order_flow_insured_person_data[insured_person_attributes][address_attributes][number]', getValue('insuredPersonAddressNumber'));
   }
 
-  if (path === '/life/proposals/beneficiaries/new') {
-    fill_in('life_quote[beneficiaries][][name]', getValue('beneficiaryName'));
-    fill_in('life_quote[beneficiaries][][relationship]', getValue('beneficiaryRelationship'));
-    fill_in('life_quote[beneficiaries][][compensation]', getValue('beneficiaryCompensation'));
+  if (path === '/life/order/beneficiaries/edit') {
+    fill_in('life_order_flow_beneficiaries[beneficiaries_attributes][0][name]', getValue('beneficiaryName'));
+    fill_in('life_order_flow_beneficiaries[beneficiaries_attributes][0][relationship]', getValue('beneficiaryRelationship'));
+    fill_in('life_order_flow_beneficiaries[beneficiaries_attributes][0][compensation]', getValue('beneficiaryCompensation'));
   }
 });
